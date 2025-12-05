0e634a76bfac3d6925ff2ddc0d04dad291d51f59
ZeroSeeker <1260812287@qq.com>
2025-12-05 20:36:46 +0800

    增加错误支持

--- fastmysql/fastmysql.py
+++ fastmysql/fastmysql.py
@@ -20,7 +20,14 @@ import decimal
 
 silence_default = True  # 默认静默参数为True
 env_file_name_default = 'mysql.env'  # 默认数据库连接环境文件名
-reconnect_errors = (ConnectionError, ConnectionAbortedError, TimeoutError)
+reconnect_errors = (
+    ConnectionError,
+    ConnectionAbortedError,
+    TimeoutError,
+    pymysql.err.ProgrammingError,
+    pymysql.err.OperationalError,
+    pymysql.err.InternalError,
+)
 default_charset = 'utf8'
 default_show_sql = False
 default_reconnect_wait = 60
